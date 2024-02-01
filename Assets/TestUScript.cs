105b1b79eab81ab38a545a9a5508b5660ec7c5a4
Happyrobot33 <matthewherber@sbcglobal.net>
2024-02-01 12:42:01 -0500

    reformat test script to organize out its calls

--- Assets/TestUScript.cs
+++ Assets/TestUScript.cs
@@ -8,12 +8,63 @@ public class TestUScript : UdonSharpBehaviour
 {
     void Start()
     {
-        Debug.Log(VRCPlayerApi.GetPlayerCount());
+
+    }
+
+    //contains all the player interactions
+    private void PlayerInteractions()
+    {
         //get the first player
         VRCPlayerApi[] players = new VRCPlayerApi[VRCPlayerApi.GetPlayerCount()];  
         VRCPlayerApi.GetPlayers(players);
         VRCPlayerApi player = players[0];
-        Debug.Log(player.isLocal);
-        Debug.Log(player.displayName);
+        //these are listed as close as I can to the order they show up in the docs
+        d(player.IsValid());
+        player.EnablePickups(true);
+        d(player.displayName);
+        d(player.isLocal);
+        d(player.isMaster);
+        d(player.GetPickupInHand(VRC_Pickup.PickupHand.Left));
+        d(player.IsOwner(gameObject));
+        d(player.IsUserInVR());
+        player.PlayHapticEventInHand(VRC_Pickup.PickupHand.Left, 1, 1, 1);
+        player.UseAttachedStation();
+        player.UseLegacyLocomotion();
+
+        //NON IMPLEMENTED METHODS
+        #region NON_IMPLEMENTED
+        player.CombatGetCurrentHitpoints();
+        player.CombatGetDestructible();
+        player.CombatSetCurrentHitpoints(1);
+        player.CombatSetDamageGraphic(1);
+        player.CombatSetMaxHitpoints(1);
+        player.CombatSetRespawn(true, 1, transform);
+        player.CombatSetup();
+        #endregion
+
+        d(VRCPlayerApi.GetCurrentLanguage());
+        d(VRCPlayerApi.GetAvailableLanguages());
+        d(VRCPlayerApi.GetPlayerById(1));
+
+        //yes, both of these literally do THE EXACT SAME THING
+        //kill me
+        d(player.playerId);
+        d(VRCPlayerApi.GetPlayerId(player));
+        player.SetPlayerTag("tag");
+        player.SetPlayerTag("tag", "tag value");
+    }
+
+
+
+
+
+
+    /// <summary>
+    /// Debug.Log wrapper, makes it so IDEs dont complain about having hanging variable calls
+    /// </summary>
+    /// <param name="a"></param>
+    private void d(object a)
+    {
+        Debug.Log(a.ToString());
     }
 }
