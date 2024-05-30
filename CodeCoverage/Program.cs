9b02c6558d2790bdf9ae0f468f490c01e727f6d7
Happyrobot33 <matthewherber@sbcglobal.net>
2024-05-30 13:54:36 -0400

    Update Program.cs

--- CodeCoverage/Program.cs
+++ CodeCoverage/Program.cs
@@ -47,7 +47,7 @@ public partial class Program
         //check if we can see the Packages folder
         Console.WriteLine("Initial Working Directory: " + workingDirectory);
         //see if the packages folder exists
-        if(!Directory.Exists(workingDirectory + @"\Packages"))
+        if(!Directory.Exists(string.Format("{0}\\Packages", workingDirectory)))
         {
             //list everything in this directory
             string[] directories = Directory.GetDirectories(workingDirectory);
