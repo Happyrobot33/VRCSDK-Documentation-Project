c5358213fc46f975f53613c75168877c4d4a910e
Happyrobot33 <matthewherber@sbcglobal.net>
2024-06-05 14:25:29 -0400

    Latest SDK

--- CodeCoverage/obj/Debug/net8.0/CodeCoverage.AssemblyInfo.cs
+++ CodeCoverage/obj/Debug/net8.0/CodeCoverage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeCoverage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faf1bc6363cb5eb3dc78bb022cc3c409ca3c53e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c5023238f58a4ba901bc5722aae0d5fa28d0704")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeCoverage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeCoverage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
